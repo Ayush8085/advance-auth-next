6304ac73187ae9731a535ac66f9a43f252ed6f26
Ayush <ayushkumar9601@gmail.com>
2024-12-30 16:08:02 +0530

    register user to db done

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,10 @@
 
 import { RegisterSchema } from "@/schema";
 import z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { error } from "console";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedData = RegisterSchema.safeParse(values);
@@ -10,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" };
+    const { name, email, password } = validatedData.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existsUser = await getUserByEmail(email);
+
+    if (existsUser) {
+        return { error: "Email already exists!" };
+    };
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    });
+
+    // TODO: Send verification email
+
+    return { success: "User created!" };
 }
