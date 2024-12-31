0b05bed7ae0800815736a4ae991d5bf53040f9e1
Ayush <ayushkumar9601@gmail.com>
2024-12-31 17:55:25 +0530

    session modified to add 'id' in it through callbacks

--- auth.ts
+++ auth.ts
@@ -4,9 +4,32 @@ import authConfig from "./auth.config";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+import { getUserById } from "./data/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     ...authConfig,
     session: { strategy: 'jwt' },
     adapter: PrismaAdapter(db),
+    callbacks: {
+        async session({ session, token }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            if (token.role && session.user) {
+                session.user.role = token.role as string;
+            }
+            return session;
+        },
+        async jwt({ token, session }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
+            return token;
+        }
+    }
 })
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -41,8 +41,8 @@ const LoginForm = () => {
         startTransition(() => {
             login(values)
                 .then((data) => {
-                    setError(data.error);
-                    setSuccess(data.success);
+                    if(data.error) setError(data.error);
+                    if(data.success) setSuccess(data.success);
                 })
         });
     }
